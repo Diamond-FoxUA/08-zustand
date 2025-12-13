72aa552e140f2ab6946e3c265d072c757a6a018f
Dmytro Farbun <dimofarb@gmail.com>
2025-12-13 17:46:31 +0200

    chore(seo): add metadata for note details page

--- app/notes/[id]/page.tsx
+++ app/notes/[id]/page.tsx
@@ -3,8 +3,39 @@ import {
   HydrationBoundary,
   QueryClient,
 } from '@tanstack/react-query';
-import { fetchNotes } from '@/lib/api';
+import { Metadata } from 'next';
+import { fetchNotes, fetchNoteById } from '@/lib/api';
 import NotesClient from '../filter/[...slug]/Notes.client';
+
+type MetadataProps = {
+  params: Promise<{ id: string }>;
+};
+
+export async function generateMetadata({ params }: MetadataProps): Promise<Metadata> {
+  const { id } = await params;
+  const note = await fetchNoteById(id);
+
+  const title = `Note: ${note.title}`;
+  const description = note.content.slice(0, 30);
+
+  return {
+    title,
+    description,
+    openGraph: {
+      title,
+      description,
+      url: `/notes/${id}`,
+      images: [
+        {
+          url: '/public/notehub-og-meta.jpg',
+          alt: 'NoteHub – modern note-taking app',
+          width: 1200,
+          height: 630
+        }
+      ]
+    }
+  };
+}
 
 export default async function Notes() {
   const queryClient = new QueryClient();
@@ -16,7 +47,7 @@ export default async function Notes() {
 
   return (
     <HydrationBoundary state={dehydrate(queryClient)}>
-      <NotesClient tag=''/>
+      <NotesClient tag="" />
     </HydrationBoundary>
   );
 }
