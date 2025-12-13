31c94ecb4ab6afc5ddff20ef7758ca2031f7e296
Dmytro Farbun <dimofarb@gmail.com>
2025-12-13 18:00:44 +0200

    chore(assets): add Roboto font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from 'next';
+import { Roboto } from 'next/font/google';
 import './globals.css';
 
 import Header from '../components/Header/Header';
@@ -7,24 +8,33 @@ import TanStackProvider from '../components/TanStackProvider/TanStackProvider';
 
 export const metadata: Metadata = {
   title: 'NoteHub',
-  description:
-    'NoteHub is a modern note-taking web app built with Next.js.',
+  description: 'NoteHub is a modern note-taking web app built with Next.js.',
   icons: {
     icon: '/favicon.svg',
   },
   authors: [{ name: 'Dmytro Farbun' }],
   openGraph: {
     title: 'NoteHub',
-    description: 'Create, tag, search, and save drafts of your notes with NoteHub.',
+    description:
+      'Create, tag, search, and save drafts of your notes with NoteHub.',
     url: 'https://08-zustand-kappa-opal.vercel.app/',
-    images: [{
-      url: '/public/notehub-og-meta.jpg',
-      alt: 'NoteHub – modern note-taking app',
-      width: 1200,
-      height: 630
-    }]
-  }
+    images: [
+      {
+        url: '/public/notehub-og-meta.jpg',
+        alt: 'NoteHub – modern note-taking app',
+        width: 1200,
+        height: 630,
+      },
+    ],
+  },
 };
+
+export const roboto = Roboto({
+  weight: ['400','600', '700'],
+  style: 'normal',
+  subsets: ['latin'],
+  display: 'swap',
+});
 
 export default function RootLayout({
   children,
@@ -35,7 +45,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>
+      <body className={roboto.className}>
         <TanStackProvider>
           <Header />
           <main>{children}</main>
