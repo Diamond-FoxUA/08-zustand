d5470ef8fbce97ae4c6fc00a4826ee9e3d8d70df
Dmytro Farbun <dimofarb@gmail.com>
2025-12-13 17:10:27 +0200

    chore(seo): add metadata and OG for 404 page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,5 +1,25 @@
+import { Metadata } from 'next';
 import Link from 'next/link';
 import css from './Home.module.css';
+
+export const metadata: Metadata = {
+  title: 'Page not found | NoteHub',
+  description: 'The page you are looking for does not exist.',
+  metadataBase: 'https://08-zustand-kappa-opal.vercel.app/',
+  openGraph: {
+    title: 'Page not found | NoteHub',
+    description: 'The page you are looking for does not exist.',
+    url: 'https://08-zustand-kappa-opal.vercel.app/',
+    images: [
+      {
+        url: '/public/notehub-og-meta.jpg',
+        width: 1200,
+        height: 630,
+        alt: 'NoteHub – page not found'
+      }
+    ]
+  }
+}
 
 const NotFound = () => {
   return (
