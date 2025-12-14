0f00f6b7a4055d463ceeb2cc1ba73f0a7b47d24f
Dmytro Farbun <dimofarb@gmail.com>
2025-12-14 21:45:20 +0200

    feat(ux): add Toaster

--- app/notes/action/create/page.tsx
+++ app/notes/action/create/page.tsx
@@ -1,12 +1,13 @@
 import css from './CreateNote.module.css';
 import NoteForm from '@/components/NoteForm/NoteForm';
-
+import { Toaster } from 'react-hot-toast';
 function CreateNote() {
   return (
     <main className={css.main}>
       <div className={css.container}>
         <h1 className={css.title}>Create note</h1>
         <NoteForm />
+        <Toaster />
       </div>
     </main>
   );
